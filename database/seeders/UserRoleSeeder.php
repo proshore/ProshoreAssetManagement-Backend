a5853aa121ebd6c600998162d54b512cf9db16fb
sabin <sabinshrestha8888@gmail.com>
2022-09-09 14:34:01 +0545

    PAM-2 seeders created

---
+++ database/seeders/UserRoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\UserRole;
+
+class UserRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        UserRole::firstOrCreate([
+            'user_id' => 1,
+            'role_id' => 1,
+        ]);
+    }
+}
