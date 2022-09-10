526f5e59254ca81386eb18be2a91524244a8ceff
sabin <sabinshrestha8888@gmail.com>
2022-09-10 21:21:26 +0545

    PAM-2 register, login & logout api routes created

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AdminController;
-use App\Http\Controllers\API\EmployeeVendorController;
-use App\Http\Controllers\API\InviteController;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +19,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::controller(AuthController::class)->prefix('users')->group(function () {
+
+    Route::post('register', 'register');
+
+    Route::post('login', 'login');
+
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::delete('logout', 'logout');
+    });
+
+});
