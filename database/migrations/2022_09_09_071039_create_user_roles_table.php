5427260512b07dc2fb6ca2de5163825677e82732
sabin <sabinshrestha8888@gmail.com>
2022-09-13 16:23:34 +0545

    PAM-2 created Traits & use it in Controller & Handler class

--- database/migrations/2022_09_09_071039_create_user_roles_table.php
+++ database/migrations/2022_09_09_071039_create_user_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,11 +14,11 @@ return new class () extends Migration {
     {
         Schema::create('user_roles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable()
+            $table->foreignId('user_id')
                 ->constrained()
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
-            $table->foreignId('role_id')->default(1)
+            $table->foreignId('role_id')
                 ->constrained()
                 ->onDelete('cascade')
                 ->onUpdate('cascade');
