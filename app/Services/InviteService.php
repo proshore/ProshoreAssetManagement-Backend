7aaf98bf777ee885b47fa0e81b72f6843bb1e077
sabin <sabinshrestha8888@gmail.com>
2022-09-29 10:44:31 +0545

    PAM-9 Added expire time in token & map const & config values

--- app/Services/InviteService.php
+++ app/Services/InviteService.php
@@ -2,20 +2,43 @@
 
 namespace App\Services;
 
+use Firebase\JWT\JWT;
 use App\Models\Invite;
 use App\Mail\InviteMail;
 use Illuminate\Support\Facades\Mail;
+use App\Constants\Invite as InviteConstant;
+use App\Http\Resources\UserResource;
 
 class InviteService
 {
-    public function generateToken(): string
+    public function generateToken(array $user): string
     {
-        return bcrypt(now()->addDays(5)->toDateTimeLocalString());
+        $issuedAt = time();
+
+        $expirationTime = time() + InviteConstant::DEFAULT_EXIPIRE_AT;
+
+        $payload = array(
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'iat' => $issuedAt,
+            'exp' => $expirationTime,
+        );
+
+        $token = JWT::encode(
+            $payload,
+            config('jwt.secret_key'),
+            config('jwt.algorithm')
+        );
+
+        return $token;
     }
 
     public function processInvite(array $validatedInviteUser): mixed
     {
-        $token = $this->generateToken();
+        $token = $this->generateToken([
+            'name' => $validatedInviteUser['name'],
+            'email' => $validatedInviteUser['email']
+        ]);
 
         $url = config('frontend.url') . '/register/' . $token . '?email=' . $validatedInviteUser['email'] . '&name=' . urlencode($validatedInviteUser['name']);
 
@@ -23,11 +46,19 @@ class InviteService
             'name' => $validatedInviteUser['name'],
             'email' => $validatedInviteUser['email'],
             'role_id' => $validatedInviteUser['role_id'],
-            'token' => $token
         ]);
 
-        Mail::to($validatedInviteUser['email'])->send(new InviteMail($url, $validatedInviteUser['name']));
+        Mail::to($validatedInviteUser['email'])
+            ->send(
+                new InviteMail(
+                    $url,
+                    $validatedInviteUser['name']
+                )
+            );
 
-        return $user;
+        return [
+            'user' => UserResource::make($user),
+            'token' => $token
+        ];
     }
 }
---
+++ config/jwt.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'secret_key' => env('JWT_SECRET_KEY'),
+
+    'algorithm' => env('JWT_ALGORITHM') ?? 'HS256'
+
+];
