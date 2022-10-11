a08a6fffcd27f411389f55fa96d7f392f2121c2e
sabin <sabinshrestha8888@gmail.com>
2022-10-11 21:53:21 +0545

    PAM-13 Define relationship methods for Invite & Role and remove trim method from User model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\{BelongsToMany, HasMany};
 
 class Role extends Model
 {
@@ -17,5 +17,10 @@ class Role extends Model
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'user_roles', 'role_id', 'user_id');
+    }
+
+    public function invite(): HasMany 
+    {
+        return $this->hasMany(Invite::class, 'role_id');
     }
 }
