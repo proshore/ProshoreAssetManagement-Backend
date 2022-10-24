b678dacd57a099a7482ea2c06910ded35d2ed833
sabin <sabinshrestha8888@gmail.com>
2022-10-24 19:37:20 +0545

    PAM-36 Added at least one symbol required validation rule in password

--- app/Http/Requests/ResetPasswordRequest.php
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -32,7 +32,7 @@ class ResetPasswordRequest extends FormRequest
                 Password::min(8)
                     ->mixedCase()
                     ->numbers()
-                    ->symbols()
+                    ->symbols(),
             ],
             'token' => ['required', 'string']
         ];
